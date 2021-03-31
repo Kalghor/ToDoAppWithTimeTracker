7a9a13e09f24a1fb9375dbcba82f92f3679da398
Paweł Barański <kalghor@gmail.com>
2021-03-31 14:11:47 +0200

    git apiKey added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-const apikey = '';
+const apikey = '7aeef90b-2b1f-4415-b5f0-ad7c1bc0c5c5';
 const apihost = 'https://todo-api.coderslab.pl';
 
-
-
+// const sections = document.querySelectorAll("section");
+// sections.forEach(el => {
+//     el.disable = true;
+//     el.classList.add("d-none");
+// })
 document.addEventListener('DOMContentLoaded', function() {
 
 });
