6530ce33c395f9f3baf482d14dfa16b34be69676
Paweł Barański <kalghor@gmail.com>
2021-04-02 12:21:42 +0200

    Partially working adding operations to single task. To improve displaying operations for each task separately

--- js/script.js
+++ js/script.js
@@ -22,10 +22,14 @@ function apiListTasks() {
 function renderTask() {
     apiListTasks()
         .then(res => {
+            console.log("---------Task----------");
+            console.log(res);
+            console.log("--------------------------");
             res.data.forEach(el => {
                 const place = document.querySelector(".placeJS");
                 const section = document.createElement("section");
                 section.className = "card mt-5 shadow-sm";
+                section.dataset.id = el.id;
                 const div = document.createElement("div");
                 div.className = "card-header d-flex justify-content-between align-items-center";
                 const divWithoutClassys = document.createElement("div");
@@ -49,14 +53,20 @@ function renderTask() {
                 div.appendChild(divWithButtons);
                 divWithButtons.appendChild(btnFinish);
                 divWithButtons.appendChild(btnDelete);
+                renderOperationsForTask(el.id);
             })
         })
 }
 
-function listOperationsForTask(taskId){
-    return fetch(apihost + "/api/tasks/" + taskId + "zadania>/operations")
+function listOperationsForTask(taskId) {
+    return fetch(apihost + "/api/tasks/" + taskId + "/operations",
+        {
+            headers: {
+                Authorization: apikey
+            }
+        })
         .then(res => {
-            if(res.ok){
+            if (res.ok) {
                 return res.json();
             } else {
                 alert("Error in method listOperationsForTask!!!")
@@ -64,9 +74,42 @@ function listOperationsForTask(taskId){
         })
 }
 
-function renderOperationsForTask(taskId){
+function renderOperationsForTask(taskId) {
+    console.log("taskId: " + taskId);
     listOperationsForTask(taskId)
         .then(res => {
+            // console.log("taskId: " + res.data.task.id);
+            console.log("---------Operations----------");
+            console.log(res);
+            console.log("-----------------------------");
+            const divHeader = document.querySelector("section");
+            const ul = document.createElement("ul");
+            ul.className = "list-group list-group-flush";
+            res.data.forEach(el => {
+                // if(taskId === res.data.task.id) {
+                    const descriptionDiv = document.createElement("div");
+                    const buttonsDiv = document.createElement("div");
+                    const button15min = document.createElement("button");
+                    const button1h = document.createElement("button");
+                    const buttonDelete = document.createElement("button");
+                    const li = document.createElement("li");
+                    button15min.className = "btn btn-outline-success btn-sm mr-2";
+                    button15min.innerText = "+15m";
+                    button1h.className = "btn btn-outline-success btn-sm mr-2";
+                    button1h.innerText = "+1h";
+                    buttonDelete.className = "btn btn-outline-danger btn-sm";
+                    buttonDelete.innerText = "Delete";
+                    li.className = "list-group-item d-flex justify-content-between align-items-center";
+                    li.appendChild(descriptionDiv);
+                    descriptionDiv.innerText = el.description;
+                    ul.appendChild(li);
+                    li.appendChild(buttonsDiv);
+                    buttonsDiv.appendChild(button15min);
+                    buttonsDiv.appendChild(button1h);
+                    buttonsDiv.appendChild(buttonDelete);
+                // }
+            })
+            divHeader.appendChild(ul);
 
         })
 }
