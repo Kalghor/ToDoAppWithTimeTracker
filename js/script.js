b0c78cc8779a9b5b5c7fdc7468cd7043df31666a
Paweł Barański <kalghor@gmail.com>
2021-04-02 12:54:17 +0200

    display of buttons in tasks has beed fixed. I need to add display time to complete the task

--- js/script.js
+++ js/script.js
@@ -22,14 +22,13 @@ function apiListTasks() {
 function renderTask() {
     apiListTasks()
         .then(res => {
-            console.log("---------Task----------");
             console.log(res);
-            console.log("--------------------------");
             res.data.forEach(el => {
                 const place = document.querySelector(".placeJS");
                 const section = document.createElement("section");
                 section.className = "card mt-5 shadow-sm";
                 section.dataset.id = el.id;
+                section.dataset.status = el.status;
                 const div = document.createElement("div");
                 div.className = "card-header d-flex justify-content-between align-items-center";
                 const divWithoutClassys = document.createElement("div");
@@ -44,14 +43,16 @@ function renderTask() {
                 section.append(div);
                 place.after(section);
                 const divWithButtons = document.createElement("div");
-                const btnFinish = document.createElement("button");
-                btnFinish.className = "btn btn-dark btn-sm";
-                btnFinish.innerText = "Finish";
                 const btnDelete = document.createElement("button");
                 btnDelete.className = "btn btn-outline-danger btn-sm ml-2";
                 btnDelete.innerText = "Delete";
                 div.appendChild(divWithButtons);
-                divWithButtons.appendChild(btnFinish);
+                if (section.dataset.status === "open") {
+                    const btnFinish = document.createElement("button");
+                    btnFinish.className = "btn btn-dark btn-sm";
+                    btnFinish.innerText = "Finish";
+                    divWithButtons.appendChild(btnFinish);
+                }
                 divWithButtons.appendChild(btnDelete);
                 renderOperationsForTask(el.id);
             })
@@ -78,38 +79,37 @@ function renderOperationsForTask(taskId) {
     console.log("taskId: " + taskId);
     listOperationsForTask(taskId)
         .then(res => {
-            // console.log("taskId: " + res.data.task.id);
-            console.log("---------Operations----------");
-            console.log(res);
-            console.log("-----------------------------");
-            const divHeader = document.querySelector("section");
             const ul = document.createElement("ul");
             ul.className = "list-group list-group-flush";
-            res.data.forEach(el => {
-                // if(taskId === res.data.task.id) {
-                    const descriptionDiv = document.createElement("div");
-                    const buttonsDiv = document.createElement("div");
-                    const button15min = document.createElement("button");
-                    const button1h = document.createElement("button");
-                    const buttonDelete = document.createElement("button");
-                    const li = document.createElement("li");
-                    button15min.className = "btn btn-outline-success btn-sm mr-2";
-                    button15min.innerText = "+15m";
-                    button1h.className = "btn btn-outline-success btn-sm mr-2";
-                    button1h.innerText = "+1h";
-                    buttonDelete.className = "btn btn-outline-danger btn-sm";
-                    buttonDelete.innerText = "Delete";
-                    li.className = "list-group-item d-flex justify-content-between align-items-center";
-                    li.appendChild(descriptionDiv);
-                    descriptionDiv.innerText = el.description;
-                    ul.appendChild(li);
-                    li.appendChild(buttonsDiv);
-                    buttonsDiv.appendChild(button15min);
-                    buttonsDiv.appendChild(button1h);
-                    buttonsDiv.appendChild(buttonDelete);
-                // }
+            const sections = document.querySelectorAll("section");
+            sections.forEach(section => {
+                res.data.forEach(operation => {
+                    if (operation.task.id === section.dataset.id) {
+                        const descriptionDiv = document.createElement("div");
+                        const li = document.createElement("li");
+                        li.className = "list-group-item d-flex justify-content-between align-items-center";
+                        li.appendChild(descriptionDiv);
+                        descriptionDiv.innerText = operation.description;
+                        ul.appendChild(li);
+                        if (section.dataset.status === "open") {
+                            const buttonsDiv = document.createElement("div");
+                            const button15min = document.createElement("button");
+                            const button1h = document.createElement("button");
+                            const buttonDelete = document.createElement("button");
+                            button15min.className = "btn btn-outline-success btn-sm mr-2";
+                            button15min.innerText = "+15m";
+                            button1h.className = "btn btn-outline-success btn-sm mr-2";
+                            button1h.innerText = "+1h";
+                            buttonDelete.className = "btn btn-outline-danger btn-sm";
+                            buttonDelete.innerText = "Delete";
+                            li.appendChild(buttonsDiv);
+                            buttonsDiv.appendChild(button15min);
+                            buttonsDiv.appendChild(button1h);
+                            buttonsDiv.appendChild(buttonDelete);
+                        }
+                        section.appendChild(ul);
+                    }
+                })
             })
-            divHeader.appendChild(ul);
-
         })
 }
